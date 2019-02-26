dd044fe9f82e76a46c28c589ec7142cfb0e7f673
ujjawal35 <ujjawal35@users.noreply.github.com>
2019-02-26 22:10:03 +0530

    Removed some bugs

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
-from  .models import Post
+from  .models import BlogEntry
 
 class PostForm(forms.ModelForm):
 
     class Meta:
-        model = Post
+        model = BlogEntry
         fields = ('title', 'text',)
--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='BlogEntry',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
