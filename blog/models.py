cc704f58db8d114f595f9378cfd29fa01ce272d3
ujjawal35 <ujjawal35@users.noreply.github.com>
2019-02-26 22:37:23 +0530

    Removed some bugs

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+class BlogEntry(models.Model):
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
+    def publishOnSite(self):
         self.published_date = timezone.now()
         self.save()
 
--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,46 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import BlogEntry
 from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+def listBlogEntries(request):
+    posts = BlogEntry.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    Post.objects.get(pk=pk)
+def BlogEntryDetails(request, pk):
+    BlogEntry.objects.get(pk=pk)
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def BlogEntryDetails(request, pk):
+    post = get_object_or_404(BlogEntry, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
+def createNewBlogEntry(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            blogEntry = form.save(commit=False)
+            blogEntry.author = request.user
+            blogEntry.published_date = timezone.now()
+            blogEntry.save()
+            return redirect('BlogEntryDetails', pk=blogEntry.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def editBlogEntry(request, pk):
+    blogEntry = get_object_or_404(BlogEntry, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, instance=blogEntry)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+            blogEntry = form.save(commit=False)
+            blogEntry.author = request.user
+            blogEntry.published_date = timezone.now()
+            blogEntry.save()
+            return redirect('BlogEntryDetails', pk=blogEntry.pk)
     else:
-        form = PostForm(request.POST, instance=post)
-        form = PostForm(instance=post)
+        form = PostForm(request.POST, instance=blogEntry)
+        form = PostForm(instance=blogEntry)
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
